9054f216113508f599cb95390c582c5f06d6cb03
sawasawa0 <sawasawaworks@gmail.com>
2017-08-24 10:36:59 +0900

    インデックス作成

--- js/countdownset.js
+++
@@ -1,8 +0,0 @@
-<script>
-$(function () {
-	var austDay = new Date();
-	austDay = new Date(austDay.getFullYear(), 7, 31);
-	$('#defaultCountdown').countdown({until: austDay});
-	$('#year').text(austDay.getFullYear());
-});
-</script>
