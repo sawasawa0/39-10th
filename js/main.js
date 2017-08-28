9fc019bf4e8b28f3898181b9dc845be7f403a77c
sawasawa0 <sawasawaworks@gmail.com>
2017-08-28 18:25:15 +0900

    暫定完成

--- js/main.js
+++ js/main.js
@@ -163,3 +163,33 @@ $(document).ready(function() {
 
   initParallax();
 });
+
+
+// ボタンの表示／非表示を切り替える関数
+function updateButton() {
+    if ($(this).scrollTop() >= 300) { // 300px以上スクロールされた
+        // ボタンを表示
+        $(".back-to-top").fadeIn();
+    } else {
+        // ボタンを非表示
+        $(".back-to-top").fadeOut();
+    }
+}
+
+$(document).ready(function() {
+    updateButton();
+
+    // スクロールされる度にupdateButtonを実行
+    $(window).scroll(updateButton);
+
+    // ボタンをクリックしたらページトップにスクロールする
+    $(".back-to-top").click(function() {
+        // 600ミリ秒かけてトップに戻る
+        $("html, body").animate({
+            scrollTop: 0,
+        }, 600);
+
+        // ボタンのhrefに遷移しない
+        return false;
+    });
+});
